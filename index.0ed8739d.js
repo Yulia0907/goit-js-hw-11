5ab4889a2dd29818bba897953f7183bc68270507
Yulia0907 <103435385+Yulia0907@users.noreply.github.com>
2022-09-25 06:13:48 +0000

    Deploying to gh-pages from @ Yulia0907/goit-js-hw-11@10b71a58ff94be02050aebaa49e0d088808f927e 🚀

---
+++ index.0ed8739d.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".search-form")};console.log(e.form),e.form.addEventListener("submit",(async function(e){e.preventDefult();const o=e.currentTarget.searchQuery.value;console.log(o)}));
+//# sourceMappingURL=index.0ed8739d.js.map
